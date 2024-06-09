85fa5e6b1fc137349ff3d42b6608a36027f15822
arpit-mittal-15 <arpitmittaldhn@gmail.com>
2024-06-10 01:14:03 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 connectToMongoDB(process.env.MONGO_URL)
 .then(()=> console.log("mongoDB connected"));
 
-app.use(cors({origin:"https://bloggers-space.vercel.app"}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
